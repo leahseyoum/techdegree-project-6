e1b6e6357376406306db1902df32b35c78836b65
Leah Seyoum <119345274+leahseyoum@users.noreply.github.com>
2024-12-15 11:30:43 -0800

    functional tests

--- src/main/java/com/teamtreehouse/techdegrees/App.java
+++ src/main/java/com/teamtreehouse/techdegrees/App.java
@@ -8,6 +8,9 @@ import com.teamtreehouse.techdegrees.exc.ApiError;
 import com.teamtreehouse.techdegrees.model.ToDo;
 import org.sql2o.Sql2o;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
@@ -52,7 +55,7 @@ public class App {
         delete("/api/v1/todos/:id", "application/json", (req, res) -> {
             int id = Integer.parseInt(req.params("id"));
             todoDao.deleteToDo(id);
-            res.status(200);
+            res.status(204);
             return "";
         });
 
@@ -68,6 +71,16 @@ public class App {
             res.status(200);
             return todoDao.findById(id);
         }, gson::toJson);
+
+        exception(ApiError.class, (exc, req, res) -> {
+            ApiError err = (ApiError) exc;
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("status", err.getStatus());
+            jsonMap.put("errorMessage", err.getMessage());
+            res.type("application/json");
+            res.status(err.getStatus());
+            res.body(gson.toJson(jsonMap));
+        });
 
         after((req, res) -> {
             res.type("application/json");
--- src/test/java/com/teamtreehouse/techdegrees/AppTest.java
+++ src/test/java/com/teamtreehouse/techdegrees/AppTest.java
@@ -2,11 +2,9 @@ package com.teamtreehouse.techdegrees;
 
 import com.google.gson.Gson;
 import com.teamtreehouse.techdegrees.dao.Sql2oToDoDao;
-import com.teamtreehouse.techdegrees.dao.ToDoDao;
 import com.teamtreehouse.techdegrees.model.ToDo;
 import com.teamtreehouse.testing.ApiClient;
 import com.teamtreehouse.testing.ApiResponse;
-//import jdk.internal.classfile.components.ClassPrinter;
 import org.junit.*;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -14,9 +12,8 @@ import spark.Spark;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AppTest{
 
@@ -64,14 +61,101 @@ public class AppTest{
     }
 
     @Test
+    public void todoCanBeCreatedSuccessfully() {
+        Map<String, Object> values = new HashMap<>();
+        values.put("name", "Test");
+        values.put("is_completed", false);
+
+        ApiResponse res = client.request("POST", "/api/v1/todos", gson.toJson(values));
+
+        ToDo created = gson.fromJson(res.getBody(), ToDo.class);
+
+        assertNotNull(created.getId());
+        assertEquals("Test", created.getName());
+        assertFalse(created.isCompleted());
+    }
+
+    @Test
+    public void multipleToDosReturnedWithFindAll() throws Exception{
+        ToDo todo = new ToDo("test", false);
+        todoDao.addToDo(todo);
+        ToDo todo2 = new ToDo("test2", false);
+        todoDao.addToDo(todo2);
+        ToDo todo3 = new ToDo("test3", false);
+        todoDao.addToDo(todo3);
+
+        ApiResponse res = client.request("GET", "/api/v1/todos");
+        ToDo[] todosArr = gson.fromJson(res.getBody(), ToDo[].class);
+
+        assertEquals(3, todosArr.length);
+    }
+
+    @Test
     public void todosCanBeAccessedById() throws Exception{
+        ToDo todo = new ToDo("test", false);
+        todoDao.addToDo(todo);
+
+        ApiResponse res = client.request("GET", "/api/v1/todos/" + todo.getId());
+        ToDo retrieved = gson.fromJson(res.getBody(), ToDo.class);
+
+        assertEquals(todo, retrieved);
+    }
+
+    @Test
+    public void todoCanBeSuccessfullyDeleted () throws Exception{
         ToDo todo = new ToDo("test", false);
         todoDao.addToDo(todo);
         int id = todo.getId();
 
-        ApiResponse res = client.request("GET", "/api/v1/todos" + todo.getId());
-        ToDo retrieved = gson.fromJson(res.getBody(), ToDo.class);
+        ApiResponse res = client.request("DELETE", "/api/v1/todos/" + todo.getId());
 
-        assertEquals(todo, retrieved);
+        assertEquals(204, res.getStatusCode());
+        assertNull(todoDao.findById(id));
+    }
+
+    @Test
+    public void deletingToDoReturnsNoContentStatus() throws Exception{
+        ToDo todo = new ToDo("test", false);
+        todoDao.addToDo(todo);
+        int id = todo.getId();
+
+        ApiResponse res = client.request("DELETE", "/api/v1/todos/" + todo.getId());
+
+        assertEquals(204, res.getStatusCode());
+    }
+
+    @Test
+    public void todoCanBySuccessfullyUpdated() throws Exception{
+        ToDo todo = new ToDo("test", false);
+        todoDao.addToDo(todo);
+        Map<String, Object> values = new HashMap<>();
+        values.put("name", "change");
+        values.put("is_completed", true);
+
+        ApiResponse res = client.request("PUT", "/api/v1/todos/" + todo.getId(), gson.toJson(values));
+        ToDo updated = gson.fromJson(res.getBody(), ToDo.class);
+
+        assertEquals("change", updated.getName());
+        assertTrue(updated.isCompleted());
+    }
+
+    @Test
+    public void updatingTodoReturnsSuccessfulStatus() throws Exception{
+        ToDo todo = new ToDo("test", false);
+        todoDao.addToDo(todo);
+        Map<String, Object> values = new HashMap<>();
+        values.put("name", "change");
+        values.put("is_completed", true);
+
+        ApiResponse res = client.request("PUT", "/api/v1/todos/" + todo.getId(), gson.toJson(values));
+
+        assertEquals(200, res.getStatusCode());
+    }
+
+    @Test
+    public void missingToDoReturns404NotFoundStatus() throws Exception{
+       ApiResponse res = client.request("GET", "/api/v1/todos/50");
+
+       assertEquals(404, res.getStatusCode());
     }
 }
