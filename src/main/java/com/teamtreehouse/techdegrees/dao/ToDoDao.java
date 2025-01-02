b07871edec14a1aae6b7e44415b6cb19a2b31b2c
Leah Seyoum <119345274+leahseyoum@users.noreply.github.com>
2025-01-02 11:40:59 -0800

    Update ToDoDao.java

--- src/main/java/com/teamtreehouse/techdegrees/dao/ToDoDao.java
+++ src/main/java/com/teamtreehouse/techdegrees/dao/ToDoDao.java
@@ -10,6 +10,5 @@ public interface ToDoDao {
     ToDo findById(int id);
     void deleteToDo(int id);
     void updateToDo(int id, String name, boolean isCompleted);
-    // edit add method to throw Dao exception
     void addToDo(ToDo toDo) throws DaoException;
 }
